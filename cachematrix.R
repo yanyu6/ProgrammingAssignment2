1bf628631244877b15a37886ca637b94d54a2e5a
Yan Yu <yanyu6@yahoo.com>
2015-09-17 21:02:49 -0500

    Get matrix inverse - modified cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -6,17 +6,20 @@
 makeCacheMatrix <- function(x = matrix()) {
   inv  <- NULL
   ## initialize the inverse matrix
+  
   set <- function (y){
     x <<- y
     inv <<- NULL
   }
   ## set function -- if new matrix, then set to NULL
+  
   get <- function() x
   ## get function -- get the matrix 
   setinverse <- function (inversematrix) inv <<- inversematrix
   ## setinverse function for the inverse matrix, no calculation
   getinverse <- function() inv
   ## getinverse function for get the inverse matrix
+  
   list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
   ## make it a function group
 }
@@ -28,6 +31,7 @@ cacheSolve <- function(x, ...) {
 
   inv <- x$getinverse()
   ## use getinverse function
+  
   if(!is.null(inv)){
     message("getting cached data")
     return(inv)
